31ad3934d35f3bb72391ad50598139571586676d
henry <henryuks129@gmail.com>
2023-06-16 06:19:48 +0100

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import picture from '../assets/mobile-design.jpg'
+const Main = () => {
+  return (
+    <div className='container text-center bg-success'>
+        <h2>Main component</h2>
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/Navbar.css'
+const Navbar = () => {
+  return (
+    <div className='container text-center bg-primary'>
+        <h2>Navbar component</h2>
+    </div>
+  )
+}
+
+export default Navbar
